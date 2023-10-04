43ae8fccf17ebc19b4a640a6a952e75a288cb2b4
Josh <joshthyng187@gmail.com>
2023-10-04 14:13:01 -0400

    class TreeNode

---
+++ BinaryTree.java
@@ -0,0 +1,27 @@
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(int val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+public class BinaryTree {
+    protected TreeNode root;
+
+    public BinaryTree() {
+        this.root = null;
+    }
+
+    public static void main(String[] args) {
+    
+    }
+}
+
+class MyBinaryTree extends BinaryTree {
+    
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+Subtask 1: Create the class BTPrinting that extends BinaryTree class. Inside this class, implement
+two recursive methods - *Mandy*
+
+Subtask 2: Create class searchBT that extends BrinaryTree class. Inside this class, implement two
+recursive methods -  *Max*
